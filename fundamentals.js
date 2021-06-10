c6dd16e7bd5d98ed8b0a342790db182f970f9251
Archana <archanasurendran12@gmail.com>
2021-06-09 23:26:05 -0600

    my works

---
+++ fundamentals.js
@@ -0,0 +1,8 @@
+
+
+//console.log("hello world")
+
+var company_name="luminar";
+var location_name="kakkanad";
+console.log(company_name,location_name);
+
---
+++ newfile.js
@@ -0,0 +1,19 @@
+//var my_name="Archana";
+//var my_age="23";
+//console.log("hai iam" +my_name + my_age+ "years old");
+
+//var state="kerala";
+//var active_cases=15000;
+//var cured=20000;
+//var death=200;
+//console.log(`in ${state} ${active_cases} new cases and ${cured} death ${death} hasbeen reported`)
+
+var company_name="luminar technolab"; //string
+var started=2000;  //number
+var is_active=true; //boolean
+var rating=5.7;     //number
+
+console.log(typeof(company_name));
+console.log(typeof(started));
+console.log(typeof(is_active));
+console.log(typeof(rating));
